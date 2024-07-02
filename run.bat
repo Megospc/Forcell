6d7ce4b9b8b85206e06b3ae6fc1feb9c47525cce
Megospc <{ID}+{username}@users.noreply.github.com>
2024-07-02 11:22:44 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-cmake -B build -G "MinGW Makefiles"
+cmake -B build -G "MinGW Makefiles" -D "CMAKE_C_COMPILER=C:\msys64\ucrt64\bin\gcc.exe" -D "CMAKE_CXX_COMPILER=C:\msys64\ucrt64\bin\g++.exe" -D "CMAKE_MAKE_PROGRAM=C:\Program Files (x86)\GnuWin32\bin\make.exe"
 cmake --build build
 xcopy .\build\Forcell.exe .\ /Y
 start ./Forcell.exe
